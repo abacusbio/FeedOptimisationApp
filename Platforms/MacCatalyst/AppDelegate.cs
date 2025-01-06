df3cbe581ffe5b201b790316e802ae8aded9e1c9
JvanTonder-AbacusBio <148156580+JvanTonder-AbacusBio@users.noreply.github.com>
2025-01-06 14:33:45 +1300

    Initial commit

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FeedOptimisationApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
